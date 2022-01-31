637229ba87e8f02d371dcd2b0100282b750664fe
JulesF <jules.fauvet@etu.univ-amu.fr>
2022-01-31 15:58:57 +0100

    début page profil

--- annuaireFrontend/src/app/profil/profil.component.ts
+++ annuaireFrontend/src/app/profil/profil.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.component.scss']
 })
 export class ProfilComponent implements OnInit {
+  Nom:string="B Goode";
+  Prenom:string="Johnny";
+  Tel:string="06 12 34 56 78";
+  Mail:string="azerty@mail.fr";
+  Adresse:string="5 rue de la Hess";
+  Code_postal:string="75001";
+  Ville:string="Paris";
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
