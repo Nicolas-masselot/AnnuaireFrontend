14f4e35a507f391bef20d9c300d81f6a0615b6be
JulesF <jules.fauvet@etu.univ-amu.fr>
2022-02-03 01:12:08 +0100

    inscription connectée au backend

--- annuaireFrontend/src/app/inscription/inscription.component.ts
+++ annuaireFrontend/src/app/inscription/inscription.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { MessageService } from '../services/message.service';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,11 +11,20 @@ import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./inscription.component.scss']
 })
 export class InscriptionComponent implements OnInit {
+  nom:string;
+  prenom:string;
+  mail:string;
+  adresse:string;
+  tel:string;
+  code_postal:string;
+  ville:string;
+  password:string;
+
   options: FormGroup;
   hideRequiredControl = new FormControl(false);
   floatLabelControl = new FormControl('auto');
 
-  constructor(fb: FormBuilder) {
+  constructor(fb: FormBuilder, private messageservice: MessageService, private router:Router) {
     this.options = fb.group({
       hideRequired: this.hideRequiredControl,
       floatLabel: this.floatLabelControl,
@@ -20,4 +32,26 @@ export class InscriptionComponent implements OnInit {
   }
   
   ngOnInit(): void {}
+
+  register() {
+    let user = Array();
+    
+    let credentials={
+      login: this.mail,
+      password: this.password,
+      role: "user"
+    }
+    console.log(credentials)
+    this.messageservice.sendMessage(environment.AUTHSERVER, 'api/v1/users/register', credentials ).subscribe(
+      (response) => {
+        user = response.data;
+        console.log(response);
+        if (response.success) {
+          this.router.navigate(["/liste_utilisateur"]);
+        } else {
+          console.log("no");
+        }
+      }
+    );
+  }
 }
